0f77416bdfa57cb9f31984c08d509b12212be340
Xuanhuy1709 <huytranghung321@gmail.com>
2025-07-30 21:35:32 +0700

    tra xong

---
+++ De2/data.js
@@ -0,0 +1,38 @@
+// data.js
+const giaoDichData = [
+  {
+    id: 1102,
+    khachHang: "Võ Hoài An",
+    nhanVien: "Mai Thục Anh",
+    soTien: "250 000",
+    ngayMua: "06 Tháng 6 2024 9:00"
+  },
+  {
+    id: 1199,
+    khachHang: "Hoàng Thị Thắng",
+    nhanVien: "Nguyễn Văn Hồng",
+    soTien: "600 000",
+    ngayMua: "06 Tháng 6 2024 9:03"
+  },
+  {
+    id: 1239,
+    khachHang: "Nguyễn Huy Quang",
+    nhanVien: "Nguyễn Văn Hồng",
+    soTien: "934 000",
+    ngayMua: "06 Tháng 6 2024 9:10"
+  },
+  {
+    id: 1677,
+    khachHang: "Huỳnh Văn Nam",
+    nhanVien: "Mai Thục Anh",
+    soTien: "150 000",
+    ngayMua: "06 Tháng 6 2024 9:20"
+  },
+  {
+    id: 1999,
+    khachHang: "Nguyễn Hữu Minh",
+    nhanVien: "Mai Thục Anh",
+    soTien: "354 000",
+    ngayMua: "06 Tháng 6 2024 9:24"
+  }
+];
