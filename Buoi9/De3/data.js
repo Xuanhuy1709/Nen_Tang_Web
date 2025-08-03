d847a5d8edfd26c4db2380e0bb101035f3efddc6
Xuanhuy1709 <huytranghung321@gmail.com>
2025-08-03 22:21:41 +0700

    đã tạm ổn

---
+++ Buoi9/De3/data.js
@@ -0,0 +1,31 @@
+// data.js
+const nhanVienData = [
+  {
+    "id": 1,
+    "ten": "Mai",
+    "hodem": "Thục Anh",
+    "diachi": "23 Hoàng Đạo Thúy, Thanh Xuân, Hà Nội",
+    "trangThai": "active" // "active" (đang hoạt động), "inactive" (ngừng)
+  },
+  {
+    "id": 2,
+    "ten": "Hoàng",
+    "hodem": "Sinh Hồng",
+    "diachi": "23 Hoàng Đạo Thúy, Thanh Xuân, Hà Nội",
+    "trangThai": "inactive"
+  },
+  {
+    "id": 3,
+    "ten": "Tính",
+    "hodem": "Mai Trung",
+    "diachi": "1411 Đường Láng, Cầu Giấy, Hà Nội",
+    "trangThai": "active"
+  },
+  {
+    "id": 4,
+    "ten": "Hồng",
+    "hodem": "Nguyễn Văn",
+    "diachi": "1411 Đường Láng, Cầu Giấy, Hà Nội",
+    "trangThai": "inactive"
+  }
+];
